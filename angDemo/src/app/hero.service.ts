050f7dbd9d91f68f26f371c787cf1920bf513d89
tli5 <litr965@gmail.com>
2017-10-18 00:03:58 -0700

    fix heroes routes

--- angDemo/src/app/hero.service.ts
+++ angDemo/src/app/hero.service.ts
@@ -10,8 +10,7 @@ export class HeroService {
     }
 
     getHero(id: number): Promise<Hero> {
-        return this.getHeroes()
-                .then(heroes => heroes[0]);
+        return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
     }
 
     getHeroesSlowly(): Promise<Hero[]> {
